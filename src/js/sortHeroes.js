24e54e663fdcb9a60006864bb11a225d2227ef56
Neto test <maleeva51@gmail.com>
2025-11-02 12:58:51 +0300

    chore: final verification - all tests pass

--- src/js/sortHeroes.js
+++ src/js/sortHeroes.js
@@ -4,5 +4,5 @@
  * @returns {Array} Отсортированный массив героев
  */
 export function sortHeroesByHealth(heroes) {
-    return heroes.slice().sort((a, b) => b.health - a.health);
-  }
+  return heroes.slice().sort((a, b) => b.health - a.health);
+}
